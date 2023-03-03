586e7a4b0bce38ae691a31fc1969eafcc0202ba8
Carina Fonseca <carina.dafonsecapinto@heig-vd.ch>
2023-03-03 14:27:31 +0100

    Commit exercice 2

--- 02-intro-d3/src/index.js
+++ 02-intro-d3/src/index.js
@@ -1,4 +1,31 @@
 import {select} from "d3-selection";
-
-// C'est ici que vous allez écrire les premières lignes avec d3.js!
-
+// Déplacer de 50px vers la droite le premier et le deuxième cercle
+select("#cercle1")
+    .attr("cx", "100")
+// Changer la couleur du deuxième cercle
+select("#cercle2")
+    .attr("fill", "red")
+select("#cercle")
+// Rajouter du texte en dessous de chaque cercle
+select("#element1")
+    .append("text")
+    .attr("x", "70")
+    .attr("y", "110")
+    .text("Du texte")
+select("#element2")
+    .append("text")
+    .attr("x", "120")
+    .attr("y", "210")
+    .text("Du texte")
+select("#element3")
+    .append("text")
+    .attr("x", "230")
+    .attr("y", "310")
+    .text("Du texte")
+// Alignez verticalement les cercles en cliquant sur le dernier cercle
+select("#cercle3")
+    .on('click', function() {
+        select(this).attr("cx", "40")
+        select("#cercle2").attr("cx", "40")
+        select("#cercle1").attr("cx", "40")
+    })
